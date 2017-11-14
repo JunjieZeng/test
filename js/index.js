95bbad312e9e62827e7ac3ce40e32f81528a743f
JunjieZeng <2451382743@qq.com>
2017-11-14 20:41:44 +0800

    2

--- js/index.js
+++ js/index.js
@@ -14,11 +14,22 @@ var index = {
     	$(".bg-shake_btn_img").on("click",function(){
     		$(this).addClass("swing")
     	})
-    	window.addEventListener('devicemotion', function(){
-    		$('.bg-shake_btn_img').addClass("swing")
-
-    	}, false);
+    	window.addEventListener('devicemotion', deviceMotionHandler, false);
     }
 };
-
+function deviceMotionHandler(eventData) {
+var acceleration = eventData.accelerationIncludingGravity;
+x = acceleration.x;
+y = acceleration.y;
+z = acceleration.z;
+if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed || Math.abs(z - lastZ) > speed) {
+    		$('.bg-shake_btn_img').addClass("swing")
+ 
+ 
+window.removeEventListener('devicemotion', deviceMotionHandler, false);
+}
+lastX = x;
+lastY = y;
+lastZ = z;
+}
 index.init();
