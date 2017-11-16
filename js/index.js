94e3d950c3943b7e933e8b9e0b86041946a93798
JunjieZeng <2451382743@qq.com>
2017-11-16 22:34:04 +0800

    12

--- js/index.js
+++ js/index.js
@@ -1,77 +1,16 @@
-var index = {
-    init: function () {
-    	$(".bg-dia_close_icon").on("click",function(){
-    		$(this).parent().hide();
-    		$(".dia").hide();
-    	})
-    	$(".bag").one("webkitAnimationEnd",function(){
-			
-    		$(".bag").removeClass("swing");
-	    	$(".bag").on("webkitAnimationEnd",function(){
-	    		$(".bag").removeClass("swing");
-	    		$('.dia-thanks').show();$('.dia').show();
-	    	})
-    	})
-    	$(".bg-shake_btn_img").on("click",function(){
-    		$(".bag").addClass("swing");
-    		index.audio.currentTime = 0;
-			index.audio.play();
-    	})
-		if (window.DeviceMotionEvent) { 
-                 window.addEventListener('devicemotion',deviceMotionHandler, false);  
-        }
-    },
-	audio:document.getElementById("shakeMusic")
-};
+﻿$(document).ready(function(){
+	$(".btn-confirm").click(function(){
+		f=1;
+  	});
+});
 
 
-
-var speed = 30;//speed
-var x = y = z = lastX = lastY = lastZ = 0;
-function deviceMotionHandler(eventData) {  
-  var acceleration =eventData.accelerationIncludingGravity;
-        x = acceleration.x;
-        y = acceleration.y;
-        z = acceleration.z;
-        if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed || Math.abs(z-lastZ) > speed) {
-			$(".bag").addClass("swing")
-    		index.audio.currentTime = 0;
-			index.audio.play();
-        }
-        lastX = x;
-        lastY = y;
-        lastZ = z;
+//弹窗 
+function showpop(id,id_bg) {
+	document.getElementById(id).style.display = 'block';
+	document.getElementById(id_bg).style.display = 'block';       
 }
-
-
-    var speed2=30;
-    window.onload=function(){
-        var demo=document.getElementById("l");
-        var demo2=document.getElementById("l2");
-        var demo1=document.getElementById("l1");
-        demo2.innerHTML=demo1.innerHTML;
-        function Marquee(){
-            if(demo.scrollTop>=demo1.offsetHeight){
-                demo.scrollTop=0;
-            }
-            else{
-                demo.scrollTop=demo.scrollTop+1;
-            }
-        }
-        var MyMar=setInterval(Marquee,speed2);
-        demo.onmouseover=function(){clearInterval(MyMar)};
-        demo.onmouseout=function(){MyMar=setInterval(Marquee,speed2)}
-    }
-
-
-
-index.init();
-
-document.addEventListener("WeixinJSBridgeReady", function () {
-    var audio = new Audio();
-    audio.src = "shake.mp3"
-    audio.play();
-    setTimeout(function(){
-    	audio.pause();
-    })
-}, false);
+function closepop(id,id_bg) {
+	document.getElementById(id).style.display = 'none';
+	document.getElementById(id_bg).style.display = 'none';
+}
---
+++ 新建文件夹/js/index.js
@@ -0,0 +1,77 @@
+var index = {
+    init: function () {
+    	$(".bg-dia_close_icon").on("click",function(){
+    		$(this).parent().hide();
+    		$(".dia").hide();
+    	})
+    	$(".bag").one("webkitAnimationEnd",function(){
+			
+    		$(".bag").removeClass("swing");
+	    	$(".bag").on("webkitAnimationEnd",function(){
+	    		$(".bag").removeClass("swing");
+	    		$('.dia-congratulation').show();$('.dia').show();
+	    	})
+    	})
+    	$(".bg-shake_btn_img").on("click",function(){
+    		$(".bag").addClass("swing");
+    		index.audio.currentTime = 0;
+			index.audio.play();
+    	})
+		if (window.DeviceMotionEvent) { 
+                 window.addEventListener('devicemotion',deviceMotionHandler, false);  
+        }
+    },
+	audio:document.getElementById("shakeMusic")
+};
+
+
+
+var speed = 30;//speed
+var x = y = z = lastX = lastY = lastZ = 0;
+function deviceMotionHandler(eventData) {  
+  var acceleration =eventData.accelerationIncludingGravity;
+        x = acceleration.x;
+        y = acceleration.y;
+        z = acceleration.z;
+        if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed || Math.abs(z-lastZ) > speed) {
+			$(".bag").addClass("swing")
+    		index.audio.currentTime = 0;
+			index.audio.play();
+        }
+        lastX = x;
+        lastY = y;
+        lastZ = z;
+}
+
+
+    var speed2=30;
+    window.onload=function(){
+        var demo=document.getElementById("l");
+        var demo2=document.getElementById("l2");
+        var demo1=document.getElementById("l1");
+        demo2.innerHTML=demo1.innerHTML;
+        function Marquee(){
+            if(demo.scrollTop>=demo1.offsetHeight){
+                demo.scrollTop=0;
+            }
+            else{
+                demo.scrollTop=demo.scrollTop+1;
+            }
+        }
+        var MyMar=setInterval(Marquee,speed2);
+        demo.onmouseover=function(){clearInterval(MyMar)};
+        demo.onmouseout=function(){MyMar=setInterval(Marquee,speed2)}
+    }
+
+
+
+index.init();
+
+document.addEventListener("WeixinJSBridgeReady", function () {
+    var audio = new Audio();
+    audio.src = "shake.mp3"
+    audio.play();
+    setTimeout(function(){
+    	audio.pause();
+    })
+}, false);
